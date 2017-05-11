48e5db58ae3cf3bccea6dc4d6aea5267855ca955
Annanda Sousa <annanda.sousa@gmail.com>
2017-05-10 23:46:13 -0300

    Tentativa de resolver o exercício.

--- exercicios/buscador.py
+++ exercicios/buscador.py
@@ -1,3 +1,6 @@
+import sys
+import unicodedata
+
 def buscar(*palavras_chave):
     """ Busca por caracteres que contenham a palavra chave em seu nome.
     Ex:
@@ -15,3 +18,17 @@ def buscar(*palavras_chave):
     tupla é o caracter e o segundo é seu nome. Assim ele pode ser utilizado no
     construtor de um dicionário
     """
+    limite = 0
+    max_unicode_value = sys.maxunicode
+    palavras_lower = [palavra.lower() for palavra in palavras_chave]
+    while limite < max_unicode_value:
+        caracter = chr(limite)
+        try:
+            unicode_name_lower = unicodedata.name(caracter).lower()
+        except ValueError:
+            pass
+        else:
+            if all([palavra in unicode_name_lower for palavra in palavras_lower]):
+                yield (caracter, unicode_name_lower.upper())
+        finally:
+            limite += 1
